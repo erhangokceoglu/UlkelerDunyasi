88cc64eaaef83af24068d8a9be5a3df6bb9ab4b7
John Doe <erhangokceoglu91@gmail.com>
2022-12-31 14:58:54 +0300

    Add project files.

---
+++ UlkelerDunyasi/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Diagnostics;
+using UlkelerDunyasi.Models;
+using UlkelerDunyasi.Models.Data;
+
+namespace UlkelerDunyasi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UygulamaDbContext _db;
+        public HomeController(UygulamaDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Ulkeler.ToList());
+        }
+
+        [ValidateAntiForgeryToken]
+        public IActionResult UlkeKaydet(Ulke ulke)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ulke.Id == 0)
+                {
+                    _db.Add(ulke);
+                    TempData["Mesaj"] = $"\"{ulke.Ad}\" adlı ülke başarıyla eklendi.";
+                }
+                else
+                {
+                    _db.Update(ulke);
+                    TempData["Mesaj"] = $"\"{ulke.Ad}\" adlı ülke başarıyla güncellendi.";
+                }
+                _db.SaveChanges();
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UlkeSil(Ulke ulke)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Remove(ulke);
+                TempData["Mesaj"] = $"\"{ulke.Ad}\" adlı ülke başarıyla silindi.";
+                _db.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UlkelerDunyasi/Models/Data/UygulamaDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace UlkelerDunyasi.Models.Data
+{
+    public class UygulamaDbContext : DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ulke> Ulkeler => Set<Ulke>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ulke>().HasData(
+                new Ulke() { Id = 1, Ad = "Türkiye" },
+                new Ulke() { Id = 2, Ad = "ABD" },
+                new Ulke() { Id = 3, Ad = "Kanada" },
+                new Ulke() { Id = 4, Ad = "Norveç" }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
